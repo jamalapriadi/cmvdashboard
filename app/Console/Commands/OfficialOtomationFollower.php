cf35d76b6d30410453100dd423432e5e517bd715
jamalapriadi <jamal.apriadi@gmail.com>
2018-10-23 22:34:02 +0700

    testing otomation follower

--- app/Console/Commands/OfficialOtomationFollower.php
+++ app/Console/Commands/OfficialOtomationFollower.php
@@ -37,6 +37,8 @@ class OfficialOtomationFollower extends Command
      */
     public function handle()
     {
+        $this->info("Please Wait...");
+
         $bu=\DB::select("select a.id, a.unit_name,
             b.id as unit_sosmed_id, b.sosmed_id, b.unit_sosmed_name, b.status_active, 
             b.unit_sosmed_account_id
@@ -58,6 +60,8 @@ class OfficialOtomationFollower extends Command
             left join unit_sosmed b on b.business_program_unit=a.id and b.type_sosmed='brand'
             where b.sosmed_id is not null
             and b.status_active='Y'");
+
+        $bar=$this->output->createProgressBar(count($bu));
         
         $sekarang=date('Y-m-d');
 
@@ -69,6 +73,8 @@ class OfficialOtomationFollower extends Command
             $unitsosmedid[]=$row->unit_sosmed_id;
 
             if($row->sosmed_id==1){
+                $this->info("Get Follower Account Twitter = ".$row->unit_name);
+
                 $list[]=array(
                     'unit_sosmed_id'=>$row->unit_sosmed_id,
                     'tanggal'=>$sekarang,
@@ -77,6 +83,8 @@ class OfficialOtomationFollower extends Command
             }
 
             if($row->sosmed_id==2){
+                $this->info("Get Follower Account Facebook = ".$row->unit_name);
+
                 $list[]=array(
                     'unit_sosmed_id'=>$row->id,
                     'tanggal'=>$sekarang,
@@ -85,6 +93,8 @@ class OfficialOtomationFollower extends Command
             }
 
             if($row->sosmed_id==3){
+                $this->info("Get Follower Account Instagram = ".$row->unit_name);
+
                 $list[]=array(
                     'unit_sosmed_id'=>$row->id,
                     'tanggal'=>$sekarang,
@@ -93,38 +103,46 @@ class OfficialOtomationFollower extends Command
             }
 
             if($row->sosmed_id==4){
+                $this->info("Get Follower Account Youtube = ".$row->unit_name);
+
                 $list[]=array(
                     'unit_sosmed_id'=>$row->id,
                     'tanggal'=>$sekarang,
                     'follower'=>\Follower::youtube($row->unit_sosmed_account_id)
                 );
             }
+
+            $bar->advance();
         }
+        
+        $bar->finish();
+
+        $this->info("Get Followers account Success, Now process to save database....");
 
         /**
          * cek di tabel unit_sosmed follower
          * berdasarkan tanggal sekarang
          * dan berdasarkan unit_sosmed_id in $unitsosmedid
          */
-        $cekfollower=\App\Models\Sosmed\Unitsosmedfollower::where('tanggal',$sekarang)
-            ->whereIn('unit_sosmed_id',$unitsosmedid)
-            ->get();
+        // $cekfollower=\App\Models\Sosmed\Unitsosmedfollower::where('tanggal',$sekarang)
+        //     ->whereIn('unit_sosmed_id',$unitsosmedid)
+        //     ->get();
 
-        if(count($cekfollower)>0){
-            $this->info("oppsss, anda tidak bisa mengisi data ini");
-        }else{
-            \DB::transaction(function() use($list){
-                foreach($list as $k=>$v){
-                    $new=new \App\Models\Sosmed\Unitsosmedfollower;
-                    $new->tanggal=$v['tanggal'];
-                    $new->unit_sosmed_id=$v['unit_sosmed_id'];
-                    $new->follower=$v['follower'];
-                    $new->insert_user='jamal.apriadi@mncgroup.com';
-                    $new->save();
-                }
-            });
+        // if(count($cekfollower)>0){
+        //     $this->info("oppsss, anda tidak bisa mengisi data ini");
+        // }else{
+        //     \DB::transaction(function() use($list){
+        //         foreach($list as $k=>$v){
+        //             $new=new \App\Models\Sosmed\Unitsosmedfollower;
+        //             $new->tanggal=$v['tanggal'];
+        //             $new->unit_sosmed_id=$v['unit_sosmed_id'];
+        //             $new->follower=$v['follower'];
+        //             $new->insert_user='jamal.apriadi@mncgroup.com';
+        //             $new->save();
+        //         }
+        //     });
 
-            $this->info("yey sukses menyimpan data");
-        }
+        //     $this->info("yey sukses menyimpan data");
+        // }
     }
 }
