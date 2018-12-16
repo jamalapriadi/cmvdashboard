cb3cff9066dfe5dba0d7548411c508944201e78f
jamalapriadi <jamal.apriadi@gmail.com>
2018-12-16 17:12:33 +0700

    menghilangkan youtube di official report

--- resources/views/sosmed/pdf/sosmed_daily_report_tv.blade.php
+++ resources/views/sosmed/pdf/sosmed_daily_report_tv.blade.php
@@ -251,7 +251,19 @@
                     </tr>
                     <tr>
                         @foreach($sosmed as $row)
-                            @if($row->id!=4)
+                            @if($row->id==1)
+                                <th class='text-center' style='background:{{$row->sosmed_color}};color:white'>{{date('d-m-Y',strtotime($kemarin))}}</th>
+                                <th class='text-center' style='background:{{$row->sosmed_color}};color:white'>Growth</th>
+                                <th class='text-center' style='background:{{$row->sosmed_color}};color:white'>{{date('d-m-Y',strtotime($sekarang))}}</th>
+                            @endif 
+
+                            @if($row->id==2)
+                                <th class='text-center' style='background:{{$row->sosmed_color}};color:white'>{{date('d-m-Y',strtotime($kemarin))}}</th>
+                                <th class='text-center' style='background:{{$row->sosmed_color}};color:white'>Growth</th>
+                                <th class='text-center' style='background:{{$row->sosmed_color}};color:white'>{{date('d-m-Y',strtotime($sekarang))}}</th>
+                            @endif 
+
+                            @if($row->id==3)
                                 <th class='text-center' style='background:{{$row->sosmed_color}};color:white'>{{date('d-m-Y',strtotime($kemarin))}}</th>
                                 <th class='text-center' style='background:{{$row->sosmed_color}};color:white'>Growth</th>
                                 <th class='text-center' style='background:{{$row->sosmed_color}};color:white'>{{date('d-m-Y',strtotime($sekarang))}}</th>
