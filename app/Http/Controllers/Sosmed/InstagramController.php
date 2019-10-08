d596ad30dc7e086df1c12a51189c55333f502697
Jamal Apriadi <jamal.apriadi@mncgroup.com>
2019-10-08 11:47:24 +0700

    test api ig and add hafiza to cc email notif

--- app/Http/Controllers/Sosmed/InstagramController.php
+++ app/Http/Controllers/Sosmed/InstagramController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Sosmed;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Http\Request;
  
 class InstagramController extends Controller
 {
